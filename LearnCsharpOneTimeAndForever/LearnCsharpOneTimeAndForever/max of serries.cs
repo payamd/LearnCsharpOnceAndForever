cffbb6f2a266b6aed86eff795d41105ac56aabe8
payamd <payam.d7@gmail.com>
2017-03-27 07:02:10 -0800

    max serries fixed

--- LearnCsharpOneTimeAndForever/LearnCsharpOneTimeAndForever/max of serries.cs
+++ LearnCsharpOneTimeAndForever/LearnCsharpOneTimeAndForever/max of serries.cs
@@ -15,7 +15,6 @@ namespace LearnCsharpOneTimeAndForever
             Console.WriteLine("Write a program and ask the user to enter a series of numbers separated by comma. Find the maximum of the numbers and display it on the console. For example, if the user enters “5, 3, 8, 1, 4\", the program should display 8");
             
             string str = adad();
-            //var array2 = Array.ConvertAll<string, int>(array, int.Parse);
             string[] words = str.Split(',');
             
           
@@ -26,14 +25,14 @@ namespace LearnCsharpOneTimeAndForever
             a.Sort();
             foreach (var number in a)
             {
-                Console.WriteLine(number);
+                Console.Write(number + ", ");
             }
 
 
             Console.ReadLine();
 
 
-
+           
         }
 
 
