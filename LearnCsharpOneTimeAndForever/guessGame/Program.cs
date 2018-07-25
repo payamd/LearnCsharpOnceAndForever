f447e1c7673b6f491b752437f9e4b344e500a7ce
payamd <payam.d7@gmail.com>
2018-07-25 20:38:02 +0430

    i have a small progress in it :dancers:

---
+++ LearnCsharpOneTimeAndForever/guessGame/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Lifetime;
+using System.Security.Cryptography;
+
+namespace guessGame
+{
+    class Program
+    {
+        public int Randomgenerator()
+        {
+            Random randomNumber = new Random();
+            return randomNumber.Next(1, 10);
+        }
+
+        static void Main()
+        {
+            var randomNumber = new guessGame.Program().Randomgenerator();
+            var life = 3;
+           
+            Console.WriteLine("select a number between 1 to 10 :D can you guess the random generated number? you have 3 tries !");
+            while (life>0)
+            {
+                var guessnumber =  int.Parse(Console.ReadLine());
+              if (guessnumber == randomNumber)
+                   Console.WriteLine("bullseye, gjj");
+              else
+              {
+                  life--;
+                    Console.WriteLine(string.Format("your life is {0}! try again.",life));
+               
+                }       
+
+            }
+
+            Console.WriteLine(string.Format("the number was {0}!", randomNumber));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ LearnCsharpOneTimeAndForever/hello world/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        public enum Orders
+        {
+            mail = 1,
+            post =2,
+            car = 3
+        }
+
+        static void Main()
+        {
+            var methodName = "car";
+            var order = Orders.mail; 
+
+            Console.WriteLine("hello world! as always ;) \n {0} , {1} ", order, (int) order);
+            Console.WriteLine((Orders) 2);
+            Console.WriteLine((int)(Orders) Enum.Parse(typeof(Orders),methodName));
+            Console.ReadLine();
+        }
+    }
+}
